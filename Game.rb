0cc2d00f46ed37c047858b6cd477c97022056f74
Viktor Lund <viktor.lund@elev.it-gymnasiet.se>
2012-11-23 11:51:11 +0100

    Added shooting and velocity on the shots

--- Game.rb
+++ Game.rb
@@ -1,12 +1,21 @@
-require 'chingu'
+require 'Chingu'
 
 class Game < Chingu::Window
 
 	# Constructor
 	def initialize
-		super
+		super 640, 480
 		self.input = {esc: :exit}
+		@background_image = Background.create
 		@player = Player.create
+	end
+end
+
+class Background < Chingu::GameObject
+	def setup
+		@x = 640/2
+		@y = 480/2
+		@image = Gosu::Image["galaxy.jpg"]
 	end
 end
 
@@ -15,37 +24,54 @@ class Player < Chingu::GameObject
 	# meta-constructor
 	def setup
 		@x, @y = 350, 400
+		@speed = 5
+		@angle = 0
 		@image = Gosu::Image["ship.png"]
 		self.input = {
 			holding_left: :left,
 			holding_right: :right,
 			holding_up: :up,
-			holding_down: :down
+			holding_down: :down,
+			holding_space: :fire
 		}
 	end
 
 	def left
-		unless @x <= 0
-			@x -= 5
+		unless @x - image.width/2 <= 0
+			@angle -= 4
 		end
 	end
 
 	def right
-		unless @x >=800
-			@x += 5
+		unless @x + image.width/2 >=800
+			@angle += 4
 		end
 	end
 
 	def up
-		unless @y <= 0
-			@y -= 5
+		unless @y - image.width/2 <= 0
+			@y -= @speed
 		end
 	end
 
 	def down
-		unless @y >= 600
-			@y += 5
+		unless @y + width/2 >= 480
+			@y += @speed
 		end
+	end
+
+	def fire
+		Laser.create(x: self.x, y: self.y, angle: self.angle)
+	end
+end
+
+class Laser < Chingu::GameObject
+	has_traits :velocity
+
+	def setup
+		@image = Gosu::Image["lazer.png"]
+		self.velocity_y = Gosu::offset_y(angle, 15)
+		self.velocity_x = Gosu::offset_x(angle, 15)
 	end
 end
 
